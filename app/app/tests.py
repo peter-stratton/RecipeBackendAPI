11fd01047b0563c924a239fe7bf61b309e9f7163
Peter Stratton <pstratton@holmansv.com>
2020-08-23 10:02:54 -0400

    added second function and test to calc

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, subtract
 
 
 class CalcTests(TestCase):
 
     def test_add_whole_numbers(self):
         self.assertEqual(add(3, 8), 11)
+
+    def test_subtract_whole_numbers(self):
+        self.assertEqual(subtract(5, 3), 2)
