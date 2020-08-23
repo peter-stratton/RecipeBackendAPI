0e4c5fc047eea1553e994cf04978d0d27a9a7107
Peter Stratton <pstratton@holmansv.com>
2020-08-23 16:20:08 -0400

    updated tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -26,6 +26,13 @@ class UserAdmin(BaseUserAdmin):
             {'fields': ('last_login',)}
         )
     )
+    add_fieldsets = (
+        (
+            None,
+            {'classes': ('wide',),
+             'fields': ('email', 'password1', 'password2')}
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
